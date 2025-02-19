2e3c70818d66378f1a5e17bfb6a671880de8fee0
rolanveroncruz <rolanveroncruz@gmail.com>
2025-02-19 12:56:24 +0800

    implemented some logging

--- api/main.py
+++ api/main.py
@@ -1,12 +1,28 @@
 from fastapi import FastAPI, Request, Response
 import json
+import os
+import logging
 
 app = FastAPI()
+# Configure logging
+log_directory = "logs"  # Directory to store logs
+log_file_path = os.path.join(log_directory, "fastapi.log")
 
+# Create the log directory if it doesn't exist
+os.makedirs(log_directory, exist_ok=True)
+
+logging.basicConfig(
+    filename=log_file_path,  # Log file path
+    level=logging.INFO,  # Minimum log level to capture (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",  # Log message format
+    datefmt="%Y-%m-%d %H:%M:%S"  # Date format
+)
+
+logger = logging.getLogger(__name__)  # Get a logger instance
 
 @app.get("/api/")
 async def root():
-
+    logger.info(f"/api called.")
     return {"whoami":"rolanvc.dev"}
 
 
@@ -16,13 +32,19 @@ async def meta_webhook(request:Request):
     Actually, it may also be the same endpoint for EVENT NOTIFICATION"""
     verify_token = "a7b3D9zX2kLqR1mN5pT8"
     mode = request.query_params.get("hub.mode", None)
+    logger.info(f"mode: {mode}")
     token = request.query_params.get("hub.verify_token", None)
+    logger.info(f"token: {token}")
     challenge = request.query_params.get("hub.challenge", None)
+    logger.info(f"challenge: {challenge}")
     if mode is None or mode != 'subscribe' or token is None or challenge is None:
+        logger.info(f"returning: 403")
         return Response(status_code=403)
     if token == verify_token:
+        logger.info(f"token and verify_token_match, returning: {challenge}")
         return Response(content=challenge, status_code=200)
     else:
+        logger.info(f"mismatch in token:{token} & verify token:{verify_token}::: {challenge}")
         return Response(status_code=403)
 
 @app.post("/webhook")
